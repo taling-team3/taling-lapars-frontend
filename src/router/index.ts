b94c7e03bc2ef30608b5c2ae243dc606e164e1a9
lee <unknown9732@naver.com>
2022-03-21 20:18:47 +0900

    feat(router):검색 페이지 경로 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,14 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/TalentDetailView.vue'),
   },
+  {
+    path: '/Search',
+    name: 'search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SearchView.vue'),
+  },
 ];
 
 const router = createRouter({
