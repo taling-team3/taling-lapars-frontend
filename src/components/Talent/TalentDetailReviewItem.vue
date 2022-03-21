dec384f95e5c1f17603db89ba8cc7f88dfb45d6a
lee <unknown9732@naver.com>
2022-03-21 19:33:24 +0900

    feat(TalentDetail):수업 상세 페이지 마크업 구현

---
+++ src/components/Talent/TalentDetailReviewItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ReviewItem">
+    <slot name="title"/>
+    <div class="flex items-center justify-center">
+      <img :src="starImgUrl" alt="star" class="h-[13px] w-[14px]"/>
+      <slot name="score"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TalentDetailReviewItem',
+  data() {
+    return {
+      starImgUrl: 'https://front-img.taling.me/Content/Images/class/icon_star_new.png',
+    };
+  },
+});
+</script>
+
+<style scoped>
+.ReviewItem {
+  @apply flex w-[100px] items-center justify-between
+}
+</style>
---
+++ src/views/TalentDetailView.vue
@@ -0,0 +1,26 @@
+<template>
+  <CommonHeader/>
+  <div class="relative w-full">
+    <main class="flex justify-center w-full mt-8 mb-48">
+      <TalentDetailContent/>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CommonHeader from '@/components/Common/CommonHeader.vue';
+import TalentDetailContent from '@/components/Talent/TalentDetailContent.vue';
+
+export default defineComponent({
+  name: 'TalentDetailView',
+  components: {
+    TalentDetailContent,
+    CommonHeader,
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
