4cd63183c96f093c76ac3cc183a71dbbd5221e4c
lee <unknown9732@naver.com>
2022-03-21 18:38:24 +0900

    feat(App.vue):CommonFooter 컴포넌트 렌더

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <router-view/>
+  <CommonFooter/>
 </template>
 
 <style></style>
+<script lang="ts">
+import CommonFooter from '@/components/Common/CommonFooter.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { CommonFooter },
+});
+</script>
