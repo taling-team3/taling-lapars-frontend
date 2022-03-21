511da93d2b98477088622c243ce08f6b42d0560a
lee <unknown9732@naver.com>
2022-03-21 19:44:13 +0900

    refactor(TalentDetail):TalentDetailView.vue 에서 TalentDetailAside.vue 렌더 하도록 수정

--- src/views/TalentDetailView.vue
+++ src/views/TalentDetailView.vue
@@ -3,6 +3,7 @@
   <div class="relative w-full">
     <main class="flex justify-center w-full mt-8 mb-48">
       <TalentDetailContent/>
+      <TalentDetailAside/>
     </main>
   </div>
 </template>
@@ -11,10 +12,12 @@
 import { defineComponent } from 'vue';
 import CommonHeader from '@/components/Common/CommonHeader.vue';
 import TalentDetailContent from '@/components/Talent/TalentDetailContent.vue';
+import TalentDetailAside from '@/components/Talent/TalentDetailAside.vue';
 
 export default defineComponent({
   name: 'TalentDetailView',
   components: {
+    TalentDetailAside,
     TalentDetailContent,
     CommonHeader,
   },
