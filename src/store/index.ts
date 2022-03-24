d7298f74c36085cc35249ca38ff8c42f203e6d75
Jee <jeehyesoo.dev@gmail.com>
2022-03-24 23:01:26 +0900

    feat(login):로그인시 store에 token 추가하여 HomeView에서 로그인 인식하도록 함

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,28 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    idToken: '',
+    name: '',
   },
   getters: {
+    isAuthenticated: (state) => !!state.idToken,
+    getName: (state) => state.name,
   },
   mutations: {
+    setIdToken: (state, idToken) => {
+      state.idToken = idToken;
+    },
+    setName: (state, name) => {
+      state.name = name;
+    },
   },
   actions: {
+    setIdToken: async ({ commit }, idToken) => {
+      commit('setIdToken', idToken);
+    },
+    setName: async ({ commit }, name) => {
+      commit('setName', name);
+    },
   },
   modules: {
   },
